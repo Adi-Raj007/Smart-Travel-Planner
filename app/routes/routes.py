e23f586afb20bc676b2879b62b58cf9a7566f3b1
Aditya Raj <subh7282@gmail.com>
2025-04-12 16:23:05 +0530

    The routes are changed a little bit like budget and place_from

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -13,14 +13,15 @@ class ItineraryResponse(BaseModel):
     place_to_visit: str
     no_of_days: int
     days: list[DayPlan]
-    budget:str
+    budget:int
 
 
 class ItineraryRequest(BaseModel):
     place_to_visit: Optional[str] = Field(default=None,description="The Place where user want to travel or want a full itinerary")
-    place_from: list[str] = Field(description="The place where to visit or explore")
+    place_from: Optional[str] = Field(description="The place where to visit or explore")
     no_of_days: Optional[int] = Field(default=None, description="Number of Days of the itinerary")
-    budget:Optional[str]
+    budget:Optional[int]=Field(default=5000,description="The total cost or budget of the trip")
+
 
 
 class FreePromptRequest(BaseModel):
